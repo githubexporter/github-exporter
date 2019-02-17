fb154dc4fa0b54fb004ffbb8c0ea7ccaac1b6fa2
Adam Pietrzycki <adam.pietrzycki@infinityworks.com>
2019-02-17 02:14:04 +0000

    Testing v4 setup

--- exporter/github.go
+++ exporter/github.go
@@ -8,18 +8,33 @@ import (
 )
 
 var query struct {
-	Viewer struct {
-		Login     githubv4.String
-		CreatedAt githubv4.DateTime
-	}
+	Repository struct {
+		Stargazers struct {
+			TotalCount githubv4.Int
+		}
+		Issues struct {
+			TotalCount githubv4.Int
+		}
+		Watchers struct {
+			TotalCount githubv4.Int
+		}
+		ForkCount githubv4.Int
+	} `graphql:"repository(owner: $owner, name: $name)"`
 }
 
 // Query GitHub API
 func Query(client *githubv4.Client) {
-	err := client.Query(context.Background(), &query, nil)
+	variables := map[string]interface{}{
+		"owner": githubv4.String("infinityworks"),
+		"name":  githubv4.String("github-exporter"),
+	}
+
+	err := client.Query(context.Background(), &query, variables)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("    Login:", query.Viewer.Login)
-	fmt.Println("CreatedAt:", query.Viewer.CreatedAt)
+	fmt.Println("Stars    - ", query.Repository.Stargazers.TotalCount)
+	fmt.Println("Issues   - ", query.Repository.Issues.TotalCount)
+	fmt.Println("Watchers - ", query.Repository.Watchers.TotalCount)
+	fmt.Println("Forks    - ", query.Repository.ForkCount)
 }
