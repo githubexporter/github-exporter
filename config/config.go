c21f8ac9d4946a7acb76da4a0ecdef295b02602c
Charlie Moog <moogcharlie@gmail.com>
2021-09-02 15:48:05 -0700

    fix: parse `ORGS` environment variable properly

--- config/config.go
+++ config/config.go
@@ -51,7 +51,7 @@ func Init() Config {
 	}
 	orgs := os.Getenv("ORGS")
 	if orgs != "" {
-		appConfig.SetOrganisations(strings.Split(repos, ", "))
+		appConfig.SetOrganisations(strings.Split(orgs, ", "))
 	}
 	users := os.Getenv("USERS")
 	if users != "" {
