656e7dc33fe72a8a14420735059555ee036c96af
Henry McConville <henry.mcconville@infinityworks.com>
2023-10-25 13:23:38 +0100

    Version 1.0.6 release

--- release-version.sh
+++ release-version.sh
@@ -9,7 +9,7 @@ git pull origin master
 version=$(cat VERSION)
 echo "version: $version"
 
-# exist if tag already exists.
+# exit if tag already exists.
 if [ $(git tag -l "$version") ]; then
   echo "tag already exists. Ensure version number has been update in VERSION."
   exit 1
@@ -21,7 +21,4 @@ if ! [[ "$version" =~ ^[0-9.]+$ ]]; then
   exit 1
 fi
 
-docker build -t githubexporter/github-exporter:latest -t githubexporter/github-exporter:$version .
-docker push githubexporter/github-exporter:latest
-docker push githubexporter/github-exporter:$version
-
+docker buildx build --platform linux/amd64 -t githubexporter/github-exporter:latest -t githubexporter/github-exporter:$version --push .
